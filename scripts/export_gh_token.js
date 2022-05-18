fea1c28057cfce77c087164cfedbd7efde6c5c88
Willem-Victor van Gerven <wgerven@starling.international>
2022-05-18 22:58:09 +0200

    gh_token on windows

--- scripts/export_gh_token.js
+++ scripts/export_gh_token.js
@@ -5,4 +5,8 @@ const { execSync } = require('child_process');
  * Set GH_TOKEN in your .env file
  */
 const GH_TOKEN = process.env.GH_TOKEN;
-execSync(`export GH_TOKEN=${GH_TOKEN}`);
+if (process.platform === 'win32') {
+  execSync(`setx GH_TOKEN ${GH_TOKEN}`);
+} else {
+  execSync(`export GH_TOKEN=${GH_TOKEN}`);
+}
